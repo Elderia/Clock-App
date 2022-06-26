92ecf602ecb4c9baeb1453b9db57b5549f1728ac
CarolynnFleming <cfcatpink@gmail.com>
2022-06-26 01:02:22 -0400

    I am rich

---
+++ Clock App/Clock App/Clock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_AppApp.swift
+//  Clock App
+//
+//  Created by Carolynn Fleming on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Clock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
